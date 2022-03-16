0429b65905898c432e2924810c506f78c5b94151
bsao17 <declic62@gmail.com>
2022-03-16 12:19:07 +0100

    add brightness control page

--- data/main.js
+++ data/main.js
@@ -9,3 +9,14 @@ const toggleButtonOff = () => {
     xhttp.open("GET", "stop", true);
     xhttp.send();
 }
+
+setInterval(() => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = () => {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("status").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "brightness", true);
+    xhttp.send();
+}, 1000)
