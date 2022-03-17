95ac612f237ab8c7e75628ee43ffaba7cf3a23b9
bsao17 <declic62@gmail.com>
2022-03-17 07:16:58 +0100

    clean code

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,32 @@ const int brightness = 34;
 
 AsyncWebServer server(80);
 
-void setup() {
+void setup()
+{
   // -----------------------------------------------------Serial
   Serial.begin(115200);
-  while(!Serial){
+  while (!Serial)
+  {
     Serial.println("\n");
   }
   // -----------------------------------------------------GPIO
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
   pinMode(brightness, INPUT);
-  // -----------------------------------------------------littleFC
-  if(!LittleFS.begin()){
+  // -----------------------------------------------------littleFS
+  if (!LittleFS.begin())
+  {
     Serial.println("LittleFS Mount Failed");
-    while(1){}
+    while (1)
+    {
+    }
   }
 
   File root = LittleFS.open("/", "r");
   File file = root.openNextFile();
 
-  while(file){
+  while (file)
+  {
     Serial.print("File: ");
     Serial.println(file.name());
     file.close();
@@ -39,7 +45,8 @@ void setup() {
   WiFi.begin(ssid, password);
   Serial.println("connexion wifi start ...");
 
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
@@ -49,33 +56,30 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   // -----------------------------------------------------Server
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/index.html", "text/html");
-  });
-  server.on("/W3-style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/W3-style.css", "text/css");
-  });
-  server.on("/main.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/main.js", "text/javascript");
-  });
-  server.on("/brightness", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(LittleFS, "/index.html", "text/html"); });
+  server.on("/W3-style.css", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(LittleFS, "/W3-style.css", "text/css"); });
+  server.on("/main.js", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(LittleFS, "/main.js", "text/javascript"); });
+  server.on("/brightness", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     int val = analogRead(brightness);
     String brightness = String(val);
-    request->send(200,  "text/plain", brightness);
-  });
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200,  "text/plain", brightness); });
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     digitalWrite(led, HIGH);
-    request->send(200);
-  });
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200); });
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     digitalWrite(led, LOW);
-    request->send(200);
-  });
+    request->send(200); });
   server.begin();
   Serial.println("server start ...");
-
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
